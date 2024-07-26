477e6d7d3365bd2db2f317638523ffc3a443c51d
Elegets <elegetsoffical@gmail.com>
2024-07-26 17:49:01 +0530

    server update

---
+++ server.js
@@ -0,0 +1,21 @@
+// server.js
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/proxy', createProxyMiddleware({
+    target: 'http://localhost', // This will be replaced dynamically
+    changeOrigin: true,
+    pathRewrite: (path, req) => {
+        return path.replace('/proxy/', '/');
+    },
+    router: function (req) {
+        const target = req.query.target;
+        return target ? `http://${target}` : 'http://localhost';
+    }
+}));
+
+app.listen(3000, () => {
+    console.log('Proxy server running on port 3000');
+});
